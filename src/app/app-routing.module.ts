9997e6856c4c1f8c329b128ad8ff6ace925bf74d
kinakeyoy <erojasr@unicartagena.edu.co>
2019-03-09 18:15:57 -0500

    Pagina de prueba de kike

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },  { path: 'kike', loadChildren: './kike/kike.module#KikePageModule' }
+
 ];
 
 @NgModule({
