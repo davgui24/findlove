c6be55875858036a119b95ba4bb6d496a9c9fc2e
breinersn <bsuarezn1@unicartagena.edu.co>
2019-03-21 13:36:12 -0500

    habitaciones, header, button name="arrow-round-back"

--- src/app/pages/habitaciones/habitaciones.page.ts
+++ src/app/pages/habitaciones/habitaciones.page.ts
@@ -1,3 +1,4 @@
+import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HabitacionesPage implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+  atras(){
+    this.navCtrl.navigateBack('/inicio');
+  }
 }
